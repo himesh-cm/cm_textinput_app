e9a27e34960678848d51582d5d2164f055d42479
Himesh Panchal <106069365+himesh-cm@users.noreply.github.com>
2022-05-26 20:03:50 +0530

    updated flutter v 2.8.1

--- lib/lib/ui/accident/accident.dart
+++ lib/lib/ui/accident/accident.dart
@@ -2,7 +2,7 @@ import 'package:cm_textinput_app/lib/ui/widgets/TextInput.dart';
 import 'package:flutter/material.dart';
 
 class AccidentWidget extends StatelessWidget {
-  const AccidentWidget({Key? key}) : super(key: key);
+  const AccidentWidget({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Column(
--- lib/lib/ui/widgets/TextInput.dart
+++ lib/lib/ui/widgets/TextInput.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class TextInput extends StatelessWidget {
-  const TextInput({Key? key}) : super(key: key);
+  const TextInput({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return const Text('TextInput');
